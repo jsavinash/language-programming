497dba7653a7ec58c5385561de241cde40deb0db
root <root@LIN-JHDSBK3.corp.capgemini.com>
2025-01-22 20:57:27 +0000

    code added

--- code/basic/datatype/dataType.py
+++ code/basic/datatype/dataType.py
@@ -11,5 +11,38 @@ def nonPrimitiveData():
     print({str: "Str"})
     print({1, 2, 3})
 
+
+def listOperation():
+    listVar = []
+    listVar.append(1)
+    listVar.append(2)
+    listVar.append(3)
+    listVar.append(4)
+    listVar.remove(2)
+    listVar.pop(0)
+    listVar.pop()
+    listVar.insert(0, 1)
+    print(listVar)
+    print(listVar[0:1])
+    print(listVar[0:2])
+    print(listVar[0:])
+    print(listVar[:1])
+    listTwo = [4, 5, 6, 7]
+    listVar.extend(listTwo)
+    print(listVar)
+    print(listVar + listTwo)
+    print(listVar * 2)
+    for x in listVar:
+        print(x)
+    for x in range(len(listVar)):
+        print(x)
+    listVar.reverse()
+    print(listVar)
+    print(listVar.sort())
+    listVar.clear()
+    print(listVar)
+
+
 primitiveData()
 nonPrimitiveData()
+listOperation()
